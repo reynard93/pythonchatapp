7217c2d73b9ad4561f6365bceb006397a05534e6
reynd93 <yylee.rey@gmail.com>
2019-06-20 13:37:11 +0800

    login done

--- application.py
+++ application.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, redirect, url_for
-
+#current_user is a proxy for user object
+from flask_login import LoginManager, login_user, current_user
 from wtform_fields import *
 from models import *
 
@@ -11,7 +12,15 @@ app.secret_key = 'replace later' # NOT SECURE WILL BE REPLACED LATER
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://cuomvdlweplpwg:80ecc365d98601348d547312a5188354195e0598491be14294331b99524ab4d0@ec2-23-21-186-85.compute-1.amazonaws.com:5432/d3uj4gf635g8c1'
 db = SQLAlchemy(app)
 
+#configure flask login refer to flask login documentation
+login = LoginManager(app)
+login.init_app(app)
 
+#loader function for when user logins, take in a userid and returns user object
+@login.user_loader
+def load_user(id):
+  # User.query.filter(id=id).first() but look below for easier approach (since retrieving by pri key but has to be int for the id)
+  return User.query.get(int(id))
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
@@ -39,8 +48,11 @@ def login():
 
   #allow login if validation succeeded
   if login_form.validate_on_submit():
-    return "Logged in, finally!"
-  
+    user_object = User.query.filter_by(username=login_form.username.data).first()
+    login_user(user_object)
+    if current_user.is_authenticated:
+      return "Logged in, finally!"
+    return "Not logged in :("
   return render_template("login.html", form=login_form)
 
 #syntax will flow in smoothly when bringing socket.io not really necessayre in current evrsion of flaask
--- models.py
+++ models.py
@@ -1,8 +1,12 @@
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
 
 db = SQLAlchemy()
 
-class User(db.Model):
+#UserMixin was added in after we imported flask_login and does not modify our db in any way
+#usermixin tells flask login about our user and mixes into our class properties/mtds we can use
+#the added properties/mtds comes from flask_login such as is_authenticated etc.
+class User(UserMixin, db.Model):
    """User model """
 
    __tablename__ = "users"
