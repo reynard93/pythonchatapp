97577bb75fa2159319a124d3d66c9c7c6c0f9ad7
reynd93 <yylee.rey@gmail.com>
2019-06-30 20:07:11 +0800

    'ready'

--- application.py
+++ application.py
@@ -1,16 +1,27 @@
-from flask import Flask, render_template, redirect, url_for
+import os
+# https://www.youtube.com/watch?v=FWU_tJqr1Po
+from time import localtime, strftime
+from flask import Flask, render_template, redirect, url_for, flash
 #current_user is a proxy for user object
-from flask_login import LoginManager, login_user, current_user
+from flask_login import LoginManager, login_user, current_user, login_required, logout_user
+from flask_socketio import SocketIO, send, emit, join_room, leave_room
 from wtform_fields import *
 from models import *
 
+#note: cookies are used here, not sessions
+
 # Configure app
 app = Flask(__name__)
-app.secret_key = 'replace later' # NOT SECURE WILL BE REPLACED LATER
+app.secret_key = os.environ.get('SECRET')
+ 
 
 #Configure database NOT SECURE WILL BE REPLACED LATER
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://cuomvdlweplpwg:80ecc365d98601348d547312a5188354195e0598491be14294331b99524ab4d0@ec2-23-21-186-85.compute-1.amazonaws.com:5432/d3uj4gf635g8c1'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 db = SQLAlchemy(app)
+
+#initiaise Flask-SocketIO
+socketio = SocketIO(app)
+ROOMS = ["lounge", "news", "games", "coding"]
 
 #configure flask login refer to flask login documentation
 login = LoginManager(app)
@@ -38,6 +49,9 @@ def index():
     user = User(username=username, password=hashed_pswd)
     db.session.add(user)
     db.session.commit()
+
+    #when login page is rendered message from flash will be passed to it, 2nd parameter is optional : the category
+    flash('Registered successfully. Please login.', 'success')
     return redirect(url_for('login')) 
 
   return render_template('index.html', form=reg_form)
@@ -50,11 +64,47 @@ def login():
   if login_form.validate_on_submit():
     user_object = User.query.filter_by(username=login_form.username.data).first()
     login_user(user_object)
-    if current_user.is_authenticated:
-      return "Logged in, finally!"
-    return "Not logged in :("
+    return redirect(url_for('chat'))
+
   return render_template("login.html", form=login_form)
 
-#syntax will flow in smoothly when bringing socket.io not really necessayre in current evrsion of flaask
+@app.route("/chat", methods=['GET', 'POST'])
+# @login_required
+def chat():
+
+  # if not current_user.is_authenticated:
+  #   flash('Please login.', 'danger')
+  #   return redirect(url_for('login')) 
+
+    #displays flashed msg on login page , not showing not authenticated on chat pg
+    # return "you do not possess the credentials" #alternative is using login_required from flask login
+  # note previous you have implemented login_required yourself under a helpers.py file
+
+  return render_template('chat.html',username=current_user.username, rooms=ROOMS)
+
+@app.route("/logout", methods=['GET'])
+def logout():
+  logout_user()
+  flash('You have logged out successfully', 'success')
+  return redirect(url_for('login'))
+
+@socketio.on('message')
+def message(data):
+  print(f"\n\n{data}\n\n")
+  send({'msg': data['msg'], 'username': data['username'], 'time_stamp': strftime('%b-%d %I:%M%p', localtime())}, 
+  room=data['room']) #send to a predefined bucket called 'message'
+
+@socketio.on('join')
+def join(data):
+  join_room(data['room'])
+  send({'msg': data['username'] + " has joined the " + data['room'] + "room."}, room=data['room'])
+
+@socketio.on('leave')
+def leave(data):
+  leave_room(data['room'])
+  send({'msg': data['username'] + " has left the " + data['room'] + "room."}, room=data['room'])
+
+#syntax will flow in smoothly when bringing socket.io not really necessay in current evrsion of flask
 if __name__ == '__main__':
-  app.run(debug=True)
+  # app.run(debug=True)
+  app.run()
